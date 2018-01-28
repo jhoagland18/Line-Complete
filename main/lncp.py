0f670fb0d2f4646dda9ed31624abdadbc2ca084e
Unknown <jhoagland@scu.edu>
2018-01-27 22:17:50 -0800

    Change delimiter to '[]'

--- main/lncp.py
+++ main/lncp.py
@@ -1,11 +1,11 @@
 #Created by Jackson Hoagland on 1/27/2018
 
 welcome_message = "Welcome to Line Complete. This program will complete repetitive typing tasks for you.\n\n" \
-                  "To begin, type your text pattern as follows using \"{}\" to indicate a variable location:\n" \
-                  "[Your text 1 here]{}[Your text 2 here]{}\n" \
+                  "To begin, type your text pattern as follows using \"[]\" to indicate a variable location:\n" \
+                  "[Your text 1 here][][Your text 2 here][]\n" \
                   "Once you have entered your pattern, enter the comma separated variables you wish to fill the pattern with.\n\n" \
                   "For example:\n" \
-                  "My favorite color is {} and my favorite number is {}.\n" \
+                  "My favorite color is [] and my favorite number is [].\n" \
                   "Green, Blue\n" \
                   "9,100\n\n" \
                   "Will output:\n" \
@@ -31,10 +31,10 @@ def main():
     while(True):
         rawTextPattern = input(enter_pattern_message)
 
-        if(rawTextPattern.endswith("{}")):
+        if(rawTextPattern.endswith("[]")):
             trailing_variable=True
 
-        textPattern = rawTextPattern.split("{}")
+        textPattern = rawTextPattern.split("[]")
 
         numVariables = len(textPattern)-1
 
